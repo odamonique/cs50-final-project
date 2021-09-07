085082370058cab905372e51217ad66c9b5c7200
Monique Oda <odamonique@gmail.com>
2021-09-07 11:43:16 +0200

    added navbar for login and favourits button and created pricelist page

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/pricelist")
+def pricelist():
+    return render_template("/pricelist.html")
