5a35525221da47719cf68a5169ffebb7b32abe02
Monique Oda <moniqueoda@MacBook-Pro-de-Monique.local>
2021-10-05 14:08:57 +0200

    added favorites and created session

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+import os
+from flask_session import Session
 
+#Configure app
 app = Flask(__name__)
+
+#Configure session
+app.config["SESSION_PERMANENT"]= False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+images=os.path.join("static", "images")
+app.config["UPLOAD_FOLDER"]= images
+chocolate_Images = os.listdir("static/images/chocolates")
+print(len(chocolate_Images))
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/favorites", methods=["GET", "POST"])
+def favorites():
+    #Ensure favorites exist
+    if "favourite" not in session:
+        session["favourite"]= {}
+
+    if request.method == "POST":
+        
+        image_path = request.form.get("id")
+        favorite_dic = session["favourite"]
+        if image_path not in favorite_dic:
+            favorite_dic[image_path] = image_path
+ 
+        print(favorite_dic)
+
+        return render_template("favorites.html")
+    
+    return render_template("favorites.html")
 
 @app.route("/pricelist")
 def pricelist():
@@ -12,7 +44,17 @@ def pricelist():
 
 @app.route("/chocolates")
 def chocolates():
-    return render_template("/chocolates.html")
+
+    # chocolate_images = os.listdir("static/images/chocolates")
+    chocolate_images = ["images/chocolates/" + images for images in chocolate_Images] 
+    # len_chocolates = len(chocolate_images)
+    # chocolate = {}
+    # for k,v in enumerate(chocolate_images):
+    #     key = k
+    #     image = v
+    #     chocolate[key] = image
+ 
+    return render_template("/chocolates.html", chocolate=chocolate_images)
 
 @app.route("/decorations")
 def decorations():
